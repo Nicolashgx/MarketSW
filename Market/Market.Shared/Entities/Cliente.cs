efdca3504d361a11083abe86a1ae0be04dd20f98
nicohernandezgg@gmail.com <nicohernandezgg@gmail.com>
2023-09-17 16:53:51 -0500

    Creacion carpeta entities y clase cliente

---
+++ Market/Market.Shared/Entities/Cliente.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Market.Shared.Entities
+{
+    public class Cliente {
+
+
+        public int id { get; set; }
+        public string name { get; set; }
+    
+    
+    
+    }
+}
