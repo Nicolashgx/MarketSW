5bdac1de744af66edb90cd7dc6531f8a5bf9d689
nicohernandezgg@gmail.com <nicohernandezgg@gmail.com>
2023-09-17 19:28:42 -0500

    migracion, instalacion de Nugets, configuracion sql

---
+++ Market/Market.API/Data/DataContext.cs
@@ -0,0 +1,23 @@
+﻿using Market.Shared.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics.Metrics;
+
+namespace Market.API.Data
+{
+    public class DataContext:DbContext
+    {
+
+        public DataContext(DbContextOptions<DataContext> options): base(options) { 
+        
+        
+        }
+
+        public DbSet<Country> Countries { get; set; } //hacer un dbset por cada entidad creada
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+    
+}
---
+++ Market/Market.Shared/Entities/Country.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Market.Shared.Entities
+{
+    public class Country
+
+    {
+
+        public int Id { get; set; } //(primary key, identity (1,1))
+
+        [Display(Name = "País")] //son etiquetas
+        [MaxLength (100, ErrorMessage = "El campo {0} debe contener {1} caracteres")] //Longitud de los caracteres del campo
+        [Required (ErrorMessage = "El campo {0} es obligatorio")]
+
+        public string? Name { get; set; } //el ? indica que hace un salto de nulos
+
+        
+
+
+
+
+
+    }
+}
